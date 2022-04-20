1b6ebdcdaea8b588f3cde8e53a28eb79d974eed1
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-20 11:28:04 +0100

    traversal algorithm

--- src/index.js
+++ src/index.js
@@ -10,3 +10,18 @@ let b = new Node("B");
 let c = new Node("C");
 let d = new Node("D");
 console.log(a);
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// traversal algorithm
+
+const printLinkedList = (head) +> {
+  let current = head;
+  while (current !== null){
+
+  }
+}
+
+printLinkedList(a);
